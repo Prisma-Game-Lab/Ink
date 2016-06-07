b9c7e34e7d9434aa1d745e3a2741ba9ad88622e2
nikopaes <nickxp51@hotmail.com>
2016-06-06 23:02:37 -0300

    +leveltest.lua
    A Loveleditor generated .lua file that contains the information of a single simple level.

---
+++ levels/1/leveltest.lua
@@ -0,0 +1,375 @@
+local table = {
+	cellSize = {
+		height = 120,
+		width = 120
+	},
+	cellsQuant = {
+		n_lines = 3,
+		n_cols = 4
+	},
+	worldSize = {
+		height = 2400,
+		width = 2400
+	},
+	layers = {
+		{
+			name = 'default',
+			objects = {
+				{
+					size = {
+						height = 120,
+						width = 120
+					},
+					name = '2',
+					position = {
+						y = 2160,
+						x = 0
+					}
+				},
+				{
+					size = {
+						height = 120,
+						width = 120
+					},
+					name = '2',
+					position = {
+						y = 2160,
+						x = 120
+					}
+				},
+				{
+					size = {
+						height = 120,
+						width = 120
+					},
+					name = '2',
+					position = {
+						y = 2160,
+						x = 240
+					}
+				},
+				{
+					size = {
+						height = 120,
+						width = 120
+					},
+					name = '2',
+					position = {
+						y = 2160,
+						x = 360
+					}
+				},
+				{
+					size = {
+						height = 120,
+						width = 120
+					},
+					name = '2',
+					position = {
+						y = 2160,
+						x = 480
+					}
+				},
+				{
+					size = {
+						height = 120,
+						width = 120
+					},
+					name = '2',
+					position = {
+						y = 2160,
+						x = 600
+					}
+				},
+				{
+					size = {
+						height = 120,
+						width = 120
+					},
+					name = '2',
+					position = {
+						y = 2160,
+						x = 720
+					}
+				},
+				{
+					size = {
+						height = 120,
+						width = 120
+					},
+					name = '2',
+					position = {
+						y = 2160,
+						x = 840
+					}
+				},
+				{
+					size = {
+						height = 120,
+						width = 120
+					},
+					name = '2',
+					position = {
+						y = 2160,
+						x = 960
+					}
+				},
+				{
+					size = {
+						height = 120,
+						width = 120
+					},
+					name = '2',
+					position = {
+						y = 2160,
+						x = 1080
+					}
+				},
+				{
+					size = {
+						height = 120,
+						width = 120
+					},
+					name = '2',
+					position = {
+						y = 2160,
+						x = 1680
+					}
+				},
+				{
+					size = {
+						height = 120,
+						width = 120
+					},
+					name = '2',
+					position = {
+						y = 2160,
+						x = 1800
+					}
+				},
+				{
+					size = {
+						height = 120,
+						width = 120
+					},
+					name = '2',
+					position = {
+						y = 2160,
+						x = 1920
+					}
+				},
+				{
+					size = {
+						height = 120,
+						width = 120
+					},
+					name = '2',
+					position = {
+						y = 2160,
+						x = 2040
+					}
+				},
+				{
+					size = {
+						height = 120,
+						width = 120
+					},
+					name = '2',
+					position = {
+						y = 2160,
+						x = 2160
+					}
+				},
+				{
+					size = {
+						height = 120,
+						width = 120
+					},
+					name = '2',
+					position = {
+						y = 2160,
+						x = 2280
+					}
+				},
+				{
+					size = {
+						height = 120,
+						width = 120
+					},
+					name = '5',
+					position = {
+						y = 2280,
+						x = 0
+					}
+				},
+				{
+					size = {
+						height = 120,
+						width = 120
+					},
+					name = '5',
+					position = {
+						y = 2280,
+						x = 120
+					}
+				},
+				{
+					size = {
+						height = 120,
+						width = 120
+					},
+					name = '5',
+					position = {
+						y = 2280,
+						x = 240
+					}
+				},
+				{
+					size = {
+						height = 120,
+						width = 120
+					},
+					name = '5',
+					position = {
+						y = 2280,
+						x = 360
+					}
+				},
+				{
+					size = {
+						height = 120,
+						width = 120
+					},
+					name = '5',
+					position = {
+						y = 2280,
+						x = 480
+					}
+				},
+				{
+					size = {
+						height = 120,
+						width = 120
+					},
+					name = '5',
+					position = {
+						y = 2280,
+						x = 600
+					}
+				},
+				{
+					size = {
+						height = 120,
+						width = 120
+					},
+					name = '5',
+					position = {
+						y = 2280,
+						x = 720
+					}
+				},
+				{
+					size = {
+						height = 120,
+						width = 120
+					},
+					name = '5',
+					position = {
+						y = 2280,
+						x = 840
+					}
+				},
+				{
+					size = {
+						height = 120,
+						width = 120
+					},
+					name = '5',
+					position = {
+						y = 2280,
+						x = 960
+					}
+				},
+				{
+					size = {
+						height = 120,
+						width = 120
+					},
+					name = '5',
+					position = {
+						y = 2280,
+						x = 1080
+					}
+				},
+				{
+					size = {
+						height = 120,
+						width = 120
+					},
+					name = '5',
+					position = {
+						y = 2280,
+						x = 1680
+					}
+				},
+				{
+					size = {
+						height = 120,
+						width = 120
+					},
+					name = '5',
+					position = {
+						y = 2280,
+						x = 1800
+					}
+				},
+				{
+					size = {
+						height = 120,
+						width = 120
+					},
+					name = '5',
+					position = {
+						y = 2280,
+						x = 1920
+					}
+				},
+				{
+					size = {
+						height = 120,
+						width = 120
+					},
+					name = '5',
+					position = {
+						y = 2280,
+						x = 2040
+					}
+				},
+				{
+					size = {
+						height = 120,
+						width = 120
+					},
+					name = '5',
+					position = {
+						y = 2280,
+						x = 2160
+					}
+				},
+				{
+					size = {
+						height = 120,
+						width = 120
+					},
+					name = '5',
+					position = {
+						y = 2280,
+						x = 2280
+					}
+				}
+			}
+		}
+	}
+}
+
+return table
