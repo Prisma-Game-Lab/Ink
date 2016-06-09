435d5496ab39625935a7e601a6c81e3421dddc89
Rudá Fraga Valle <rudavalle@gmail.com>
2016-06-09 09:20:14 -0300

    Changed the way the stats screen is loaded for both the level_loader and the load_loveleditor branches the latest being the most right one

---
+++ src/stats_screen.lua
@@ -0,0 +1,11 @@
+function showStats()
+  love.graphics.setColor(155, 155, 155, 100)
+  x1,y1,x2,y2,x3,y3,x4,y4 = cam:getCamCorners()
+  w = love.graphics.getWidth()
+  h = love.graphics.getHeight()
+  love.graphics.rectangle('fill', x1 + w/2, y1+h/2, 1000, 700)
+  love.graphics.setColor(155, 0, 0, 255)
+  love.graphics.printf("Level: " ..curr_level.. "\n\nHP: " ..math.floor(player.hp).. "\n\nTime: " ..string.sub(time,1,4).. "\n", x1 + w/2, y1+h/2 + 50, 1000, 'center')
+  love.graphics.setColor(155, 155, 155)
+  
+end
