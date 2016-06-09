a1e5bea71ed6b49d99dc6de32bf408c2075e9847
Rudá Fraga Valle <rudavalle@gmail.com>
2016-06-09 09:20:14 -0300

    Changed the way the stats screen is loaded for both the level_loader and the load_loveleditor branches the latest being the most right one

--- src/level_manager.lua
+++ src/level_manager.lua
@@ -8,6 +8,7 @@ local level_manager = {}
 require "src/player"
 require "src/enemy"
 require "src/camera"
+require "src/stats_screen"
 require "lib/middleclass"
 local bump = require 'lib/bump'
 local cron = require 'lib/cron'
@@ -344,7 +345,7 @@ function level_manager.draw()
    -- dlgBox:draw(cam)
    
    if level_manager.isFinished then
-     level_manager.showStats()
+     showStats(i)
    end
    
     
@@ -383,16 +384,6 @@ function level_manager.reset()
   
 end
 
-function level_manager.showStats()
-  love.graphics.setColor(155, 155, 155, 100)
-  x1,y1,x2,y2,x3,y3,x4,y4 = cam:getCamCorners()
-  w = love.graphics.getWidth()
-  h = love.graphics.getHeight()
-  love.graphics.rectangle('fill', x1 + w/2, y1+h/2, 1000, 700)
-  love.graphics.setColor(155, 0, 0, 255)
-  love.graphics.printf("Level: " ..tostring(curr_level).. "\n\nHP: " ..math.floor(player.hp).. "\n\nTime: " ..string.sub(time,1,4).. "\n", x1 + w/2, y1+h/2 + 50, 1000, 'center')
-  love.graphics.setColor(155, 155, 155)
-  
-end
+
 
 return level_manager
